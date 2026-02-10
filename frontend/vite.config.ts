95697d07f40ec9ab453d79b0de2022be468ab2fc
Eric Huang <incarnation2025@Erics-MacBook-Pro.local>
2026-02-09 21:17:50 -0800

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: true,
     proxy: {
       "/api": {
         target: "http://localhost:8000",
@@ -14,6 +15,10 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  preview: {
+    host: "0.0.0.0",
+    allowedHosts: true,
   },
 });
 
