6d6f21d5044c996fa01fedc3ecc0b8ea3a900a1e
Eric Huang <incarnation2025@Erics-MacBook-Pro.local>
2026-03-09 23:09:20 -0700

    remove backtest

--- backend/spx_backend/backtest/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Backtest package exports for engine configuration and result types."""
-__all__ = ["BacktestConfig", "BacktestEngine", "BacktestResult"]
-
-from spx_backend.backtest.engine import BacktestConfig, BacktestEngine, BacktestResult
-
